3981344e5afc362f36bc9dc28e91da6fedc490b9
Dorian Rudolph <91009957+dorianrd@users.noreply.github.com>
2024-10-02 16:17:35 +0200

    parameterizedtest

--- src/test/java/de/fhdw/std/CheckingAccountTest.java
+++ src/test/java/de/fhdw/std/CheckingAccountTest.java
@@ -1,6 +1,11 @@
 package de.fhdw.std;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static de.fhdw.std.assertions.BankAccountAssert.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -22,6 +27,22 @@ class CheckingAccountTest {
 
         assertThatThrownBy(() -> account.deposit(-100))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValuesForWithdraw")
+    void ParamwithdrawAvailableAmount(String id, double balance, double limit, double fee, double amount, double expected) {
+        CheckingAccount account = new CheckingAccount(id, balance, limit, fee);
+        account.withdraw(amount);
+        assertThat(account).hasBalance(expected);
+    }
+
+    private static Stream<Arguments> provideValuesForWithdraw() {
+        return Stream.of(
+            Arguments.of("1", 1000, 500, 5, 200, 800),
+            Arguments.of("2", 1000, 500, 5, 1200, -205),
+            Arguments.of("3", 0, 500, 5, 100, -105)
+        );
     }
 
     @Test
